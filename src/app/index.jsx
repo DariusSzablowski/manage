ba8e7495e18b48cde75e7d9f054c58a0925c9a72
DariusSzablowski <darius.szablowski@gmx.de>
2016-07-29 23:24:30 +0200

    Styles work/ Navigation set up created

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import Navigation from './navigation.jsx';
+
 class App extends React.Component {
   render () {
-    return <p>The manage app works with React now!!!</p>;
+    return  (
+      <div>
+        <Navigation />
+        <p>The manage app works with React now!!!</p>
+      </div>
+    );
   }
 }
 
---
+++ src/app/navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+
+  /*constructor(props) {
+    super(props);
+    //...
+  }*/
+
+  render() {
+    return (
+      <div>The Navigation</div>
+    );
+  }
+}
+
+export default Navigation;
