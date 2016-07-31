42dda5c072d26b92f6aab8db20399ab85385cc1b
DariusSzablowski <darius.szablowski@gmx.de>
2016-07-31 13:36:26 +0200

    Styles now Re-set-up using Radium

--- src/app/navigation.jsx
+++ src/app/navigation.jsx
@@ -1,3 +1,4 @@
+import Radium from 'radium';
 import React from 'react';
 
 class Navigation extends React.Component {
@@ -9,9 +10,25 @@ class Navigation extends React.Component {
 
   render() {
     return (
-      <div>The Navigation</div>
+      <div style={styles.test}>
+          <ul style={styles.ul}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Menu 1</a></li>
+            <li><a href="#">Menu 2</a></li>
+            <li><a href="#">Menu 3</a></li>
+          </ul>
+      </div>
     );
   }
 }
 
-export default Navigation;
+var styles = {
+  test: {
+    background: '#00f00f',
+  },
+  ul: {
+    margin: '0px',
+  }
+};
+
+export default Radium(Navigation);
